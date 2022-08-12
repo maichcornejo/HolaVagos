9922868690897c379a28277e7eeeeff5f38faa9f
maichcornejo <108956214+maichcornejo@users.noreply.github.com>
2022-08-12 17:29:16 -0300

    Add files via upload

---
+++ para.pas
@@ -0,0 +1,19 @@
+program ParaCuadrados;
+    var 
+        A: integer;
+    begin
+        writeln('Ingrese el valor de A: ');
+        readln(a);
+        if (A<=178) then
+            B= a*a;
+            writeln('El cuadrado de', a, 'es', b)
+            begin
+                if (a mod 2=0) then
+                    writeln(a,' es par')
+                else 
+                    writeln(a, ' es impar')
+            end
+            else
+            writeln('Numero muy grande')
+            end;
+    end.
